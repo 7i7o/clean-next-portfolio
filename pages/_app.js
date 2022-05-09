c82f1190bc5ec352a43a0bcfc28b9ade7fa7f6cc
7i7o <mparij@gmail.com>
2022-05-08 21:47:17 -0300

    Simple changes, CSS, ConnectButton theme & favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,57 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Adding RainbowKit for a Sign In With Ethereum experience
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+  lightTheme,
+  darkTheme,
+} from '@rainbow-me/rainbowkit';
+import { chain, createClient, WagmiProvider } from 'wagmi';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.rinkeby],
+  [
+    apiProvider.alchemy(process.env.ALCHEMY_ID),
+    apiProvider.fallback()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  // appName: 'My RainbowKit App',
+  appName: process.env.NEXT_PUBLIC_APP_NAME,
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+const  MyApp = ({ Component, pageProps }) => {
+  return (
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        coolMode
+        theme={{
+          lightMode: lightTheme(),
+          darkMode: darkTheme(),
+        }}
+        appInfo={{
+          appName: process.env.NEXT_PUBLIC_APP_NAME,
+          // learnMoreUrl: 'https://learnaboutcryptowallets.example',
+        }}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiProvider>
+  );
 }
 
 export default MyApp
