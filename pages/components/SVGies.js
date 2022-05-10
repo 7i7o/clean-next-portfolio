4a5373dca93c7d94341841a7cd75ea454ba36a2b
7i7o <mparij@gmail.com>
2022-05-10 16:29:52 -0300

    Got SVGies working

---
+++ pages/components/SVGies.js
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+import { keccak256 } from "ethers/lib/utils"
+
+const SVGies = ({address, width, height}) => {
+
+    const getRGBAString = (hexString) => {
+        const data = hexString.length == 8 ? hexString : hexString.length == 6 ? hexString + 'ff' : 'ffffffff'
+        const r = parseInt(data.slice(0, 2), 16)
+        const g = parseInt(data.slice(2, 4), 16)
+        const b = parseInt(data.slice(4, 6), 16)
+        const a = 0.5 + parseInt(data.slice(6, 8), 16) / 512
+        return `rgba(${r},${g},${b},${a})`
+    }
+
+    const getColors = (hexData) => {
+        const colors = hexData.slice(2).match(/.{1,8}/g) || [];
+        return colors.map( hex => [hex,getRGBAString(hex)])
+    }
+
+    // Creates a path from an array with all the control points defined
+    const getPath = (arr) => {
+
+        // First coordinates are the path start point
+        let path = `M${arr[0]} ${arr[1]}`
+
+        // Every 3 coordinates, we create a Cubic Bézier Curve
+        for (let i = 2; i < arr.length - 2; i += 6) {
+            path += `C${arr[i]} ${arr[i + 1]} ${arr[i + 2]} ${arr[i + 3]} ${arr[i + 4]} ${arr[i + 5]}`
+        }
+
+        // We have 1 last coordinate that we use as control point for a Quadratic Bézier Curve
+        path += `Q${arr[arr.length - 2]} ${arr[arr.length - 1]} ${arr[0]} ${arr[1]}z`
+
+        return path
+    }
+
+    const softenCurves = (coordArray) => {
+        // From an array of 40 numbers (20 coordinates),
+        // generate an array of 58 numbers to soften Cubic Beziér Curves
+
+        // First 4 coordinates, are the same
+        const arr = coordArray.slice(0, 4)
+
+        // Every 4 coordinates we add 1 so the curves don't have hard edges
+        for (let i = 4; i < coordArray.length; i += 4) {
+            arr.splice(arr.length, 0, ...coordArray.slice(i, i + 4))
+            arr.push(2 * coordArray[i + 2] - coordArray[i], 2 * coordArray[i + 3] - coordArray[i + 1])
+        }
+        // The last added coordinates can be used for a Quadratic bezier curve at the end
+
+        return arr
+    }
+
+    const getPaths = (ethAddress) => {
+        const numArray = ethAddress.substring(2).split('').map(
+            // We 'shift' coordinates by 8 to center them inside
+            // a 32x32 grid with 'hex' values
+            char => parseInt(char, 16) + 8
+        )
+
+        const arr = softenCurves(numArray)
+
+        // We create a (vertical) symmetric set of coordinates
+        // to give the SVG an easier way to be remembered
+        let symArr = arr.map((val, index) => (index % 2) ? val : 32 - val)
+
+        return [getPath(arr), getPath(symArr)]
+
+    }
+
+    const paths = getPaths(address)
+    const hash = keccak256(address)
+    const colors = getColors(hash)
+    // console.log(colors)
+
+    return ((!paths || !colors ) ?
+        <p>Loading SVGies...</p> :
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width={width} height={height}>
+
+            <radialGradient id={`${colors[0][0]}${colors[1][0]}`}>
+                <stop offset="0" stopColor={colors[0][1]} />
+                <stop offset=".95" stopColor={colors[1][1]} />
+                <stop offset="1" stopColor="transparent" />
+            </radialGradient>
+            {/* <rect width="32" height="32" opacity=".5" fill={`url(#${colors[0][0]}${colors[1][0]})`}/> */}
+            <circle cy={16} cx={16} r={16} opacity=".5" fill={`url(#${colors[0][0]}${colors[1][0]})`}/>
+
+            <linearGradient id={`${colors[2][0]}${colors[3][0]}${colors[2][0]}`}>
+                <stop offset="0" stopColor={colors[2][1]} />
+                <stop offset=".5" stopColor={colors[3][1]} />
+                <stop offset="1" stopColor={colors[2][1]} />
+            </linearGradient>
+            <linearGradient id={`${colors[3][0]}${colors[2][0]}${colors[3][0]}`}>
+                <stop offset="0" stopColor={colors[3][1]} />
+                <stop offset=".5" stopColor={colors[2][1]} />
+                <stop offset="1" stopColor={colors[3][1]} />
+            </linearGradient>
+            <path
+                fill={`url(#${colors[2][0]}${colors[3][0]}${colors[2][0]})`}
+                strokeWidth={.1}
+                stroke={`url(#${colors[3][0]}${colors[2][0]}${colors[3][0]})`}
+                d={`${paths[0]}${paths[1]}`}
+            />
+
+            <linearGradient id={`${colors[4][0]}${colors[3][0]}${colors[2][0]}`}>
+                <stop offset="0" stopColor={colors[4][1]} />
+                <stop offset=".5" stopColor={colors[3][1]} />
+                <stop offset="1" stopColor={colors[2][1]} />
+            </linearGradient>
+            {/* <path fill={`url(#${colors[4][0]}${colors[3][0]}${colors[2][0]})`} d={paths[1]} /> */}
+        </svg>
+    )
+
+}
+
+export default SVGies
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 // Using RainbowKit to have a ConnectButton for SIWE
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import SVGies from './components/SVGies';
 
 const Home = () => {
 
-  const exampleAddress = '0xa8C93d8B2714e11f1f7Bfb54119bd9B823051AF7';
+  const exampleAddress1 = '0xC53128eAe55d64C2bD70F842247a0E8D27647241'; // Wave Portal
+  const exampleAddress2 = '0xa8B1b015E007D588cdD5DFfe61D0d8ec8fC75359'; // Ape Waver
+  const exampleAddress3 = '0xDEBD7a625a4666419Dbd6C3134088307F5D273c8'; // CCC
+  const exampleAddress4 = '0x45ee33C1806a9e6976d0985246ae839Ea7Fc3dB4'; // SSBM
 
-  const hex2int = (hexChar) => parseInt(hexChar, 16);
-
-  const getColor = (hexString) => {
-    const data = hexString.length == 4 ? hexString: hexString.length == 3 ? hexString + 'f' : 'ffff'
-    const r = hex2int(data.charAt(0)) + hex2int(data.charAt(0)) * 16;
-    const g = hex2int(data.charAt(1)) + hex2int(data.charAt(1)) * 16;
-    const b = hex2int(data.charAt(2)) + hex2int(data.charAt(2)) * 16;
-    const opacity = 0.5 + parseInt(data.charAt(3),16) / 32;
-    return `rgba(${r},${g},${b},${opacity})`
-  }
-
-  const getBezierCurve = (hexString) => {
-    if (hexString.length != 6) return;
-    return `Q ${hex2int(hexString.charAt(0))} ${hex2int(hexString.charAt(1))} ${hex2int(hexString.charAt(2))} ${hex2int(hexString.charAt(3))} ${hex2int(hexString.charAt(4))} ${hex2int(hexString.charAt(5))}`
-  }
-
-  const getPath = (ethAddress) => {
-    const str = ''
-    console.log(ethAddress.substring(2,8))
-    console.log(ethAddress.substring(8,14))
-    console.log(ethAddress.substring(14,20))
-    console.log(ethAddress.substring(20,26))
-    console.log(ethAddress.substring(26,32))
-    console.log(ethAddress.substring(32,38))
-    str += getBezierCurve(ethAddress.substring(2,8))
-    str += getBezierCurve(ethAddress.substring(8,14))
-    str += getBezierCurve(ethAddress.substring(14,20))
-    str += getBezierCurve(ethAddress.substring(20,26))
-    str += getBezierCurve(ethAddress.substring(26,32))
-    str += getBezierCurve(ethAddress.substring(32,38))
-    
-    return str
-  }
 
   return (
     <div className={styles.container}>
@@ -53,14 +24,16 @@ const Home = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hi! I&apos;m Matias
+          {/* Hi! I&apos;m Matias */}
+          SVGies
         </h1>
 
         <h2 >
-          I&apos;m a enthusiastic developer with a soft spot for web3
+          {/* I&apos;m a enthusiastic developer with a soft spot for web3 */}
+          A colorful SVG representation of your ethereum address
         </h2>
 
-        <ConnectButton
+        {/* <ConnectButton
           chainStatus="icon"
           accountStatus={{
             smallScreen: 'avatar',
@@ -70,16 +43,12 @@ const Home = () => {
             smallScreen: false,
             largeScreen: true
           }}
-        />
-        <h3>
-          {getColor(exampleAddress.substring(2,6))}
-        </h3>
-        <h3>
-          {getColor(exampleAddress.substring(38))}
-        </h3>
-        <h3>
-          {getPath(exampleAddress)}
-        </h3>
+        /> */}
+        <div className="SVGie"><SVGies address={exampleAddress1} width="200"/></div>
+        <div className="SVGie"><SVGies address={exampleAddress2} width="200"/></div>
+        <div className="SVGie"><SVGies address={exampleAddress3} width="200"/></div>
+        <div className="SVGie"><SVGies address={exampleAddress4} width="200"/></div>
+        
         {/* 
         <p className={styles.description}>
           Get started by editing{' '}
