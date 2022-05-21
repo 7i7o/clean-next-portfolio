77d4c85f7297606cc24d2b892ce6cc818b242ee1
7i7o <mparij@gmail.com>
2022-05-20 23:27:39 -0300

    Moved to ChakraUI + RainbowKit

--- pages/_app.js
+++ pages/_app.js
@@ -1,67 +1,39 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../theme'
 
-// import { useState, useEffect } from 'react'
+import '@rainbow-me/rainbowkit/styles.css';
+import { apiProvider, configureChains, getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
+import { chain, createClient, WagmiConfig, WagmiProvider } from 'wagmi';
 
-import { Provider, chain, createClient, defaultChains } from 'wagmi'
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+const { chains, provider } = configureChains(
+  // [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [chain.polygon],
+  [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
+);
 
-import { providers } from 'ethers';
-import reactDom from 'react-dom'
+const { connectors } = getDefaultWallets({ appName: 'SVGies', chains });
 
+const wagmiClient = createClient({ autoConnect: true, connectors, provider })
 
-// API key for Ethereum node
-// Two popular services are Alchemy (alchemy.com) and Infura (infura.io)
-const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID
-
-const chains = defaultChains
-const defaultChain = chain.mainnet
-
-
-const  MyApp = ({ Component, pageProps }) => {
-
-  // const [client, setClient] = useState()
-
-  // Set up connectors on page load
-  const client = createClient({
-    autoConnect: true,
-    connectors({ chainId }) {
-      const chain = chains.find((x) => x.id === chainId) ?? defaultChain
-      const rpcUrl = chain.rpcUrls.alchemy
-        ? `${chain.rpcUrls.alchemy}/${alchemyId}`
-        : chain.rpcUrls.default
-      return [
-        new MetaMaskConnector({ chains }),
-        new CoinbaseWalletConnector({
-          chains,
-          options: {
-            appName: 'wagmi',
-            chainId: chain.id,
-            jsonRpcUrl: rpcUrl,
-          },
-        }),
-        new WalletConnectConnector({
-          chains,
-          options: {
-            qrcode: true,
-            rpc: { [chain.id]: rpcUrl },
-          },
-        }),
-        new InjectedConnector({
-          chains,
-          options: { name: 'Injected' },
-        }),
-      ]
-    },
-  })
-
+function MyApp({ Component, pageProps }) {
   return (
-    <Provider client={client}>
-        <Component {...pageProps} />
-    </Provider>
-  );
+
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={darkTheme({
+        accentColor: '#7928CA',
+        accentColorForeground: 'white',
+        borderRadius: 'large',
+        fontStack: 'system',
+      })} >
+
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+
+      </RainbowKitProvider>
+    </WagmiProvider>
+
+  )
 }
 
 export default MyApp
