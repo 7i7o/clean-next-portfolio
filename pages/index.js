8481c3cbe640f08beb3b2ea1b5ed009610f41340
7i7o <mparij@gmail.com>
2022-05-21 01:02:31 -0300

    Moved js file without comps outside pages folder

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import { Box, Center, Divider, Heading, Skeleton, Wrap, WrapItem } from '@chakra-ui/react'
+import { Box, Center, Divider, Heading, Skeleton, Stack, Text, VStack, Wrap, WrapItem } from '@chakra-ui/react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
 import Head from 'next/head'
 
 import SVGies from './components/SVGies';
-import { wallets } from './constants/wallets.js';
+import { wallets } from '../constants/wallets.js';
 
 const Home = () => {
 
@@ -30,21 +30,31 @@ const Home = () => {
             SVGies
           </Heading>
         </Center>
-        <Center w='100%' h='4em' >
+        <Center>
+          <VStack >
+            <Center align='center' pl='3em' pr='3em'>SVGies are a unique visual representation of your wallet address</Center>
+            <Center align='center' pl='3em' pr='3em'>You can only mint <b>&nbsp; ONE &nbsp;</b> for they are a 1:1 representation of your wallet</Center>
+            <Center align='center' pl='3em' pr='3em'>They are stored on-chain in the Polygon mainnet network</Center>
+            <Center align='center' pl='3em' pr='3em'>You can think of them as Blockies from the future</Center>
+            <Center align='center' pl='3em' pr='3em'>Drawn as generative art in a beautifully detailed and colorful SVG</Center>
+            <Center align='center' pl='3em' pr='3em'>Using Quadratic and Cubic Bézier curves and leveraging our brain&apos;s ability to remember vertical simmetry</Center>
+          </VStack>
+        </Center>
+        <Center w='100%' h='18em' >
+          {(isLoading && <Skeleton><Box w='200' h='200'></Box></Skeleton>)}
+          {(isError && <>Error Loading Account</>)}
+          {(!isLoading && !isError && !data && <>Please Connect Wallet to Mint or View your SVGie</>)}
+          {(!isLoading && !isError && data &&
+            <Center boxSize={240} bg='#ffffffcc' borderRadius={'.75em'}>
+              <SVGies address={data?.address} width={200} height={200} fill={`#000`} />
+            </Center>)}
+        </Center>
+        <Center w='100%' h='6em' pb='2em'>
           <ConnectButton
             accountStatus={{ smallScreen: 'avatar', largeScreen: 'address', }}
             chainStatus={{ smallScreen: 'icon', largeScreen: 'name', }}
             showBalance={{ smallScreen: false, largeScreen: true, }}
           />
-        </Center>
-        <Center w='100%' h='20em' >
-          {(isLoading && <Skeleton><Box w='200' h='200'></Box></Skeleton>)}
-          {(isError && <>Error Loading Account</>)}
-          {(!isLoading && !isError && !data && <>Please Connect Wallet to View or Mint a SVGie</>)}
-          {(!isLoading && !isError && data &&
-            <Center boxSize={240} bg='#ffffffcc' borderRadius={'.75em'}>
-              <SVGies address={data?.address} width={200} height={200} fill={`#000`} />
-            </Center>)}
         </Center>
         <Divider />
 
@@ -76,7 +86,7 @@ const Home = () => {
         <Divider />
 
         <Center h='7em' >
-            2022 MIT License &copy; 7i7o
+          2022 MIT License &copy; 7i7o
         </Center>
       </main>
     </div >
