e6391d0f486bf63175e6c317cdbbc62fae251404
7i7o <mparij@gmail.com>
2022-05-21 22:18:34 -0300

    Modified sizes of SVGies

--- pages/index.js
+++ pages/index.js
@@ -32,22 +32,24 @@ const Home = () => {
         </Center>
         <Center>
           <VStack >
-            <Center align='center' pl='3em' pr='3em'>SVGies are a unique visual representation of your wallet address</Center>
-            <Center align='center' pl='3em' pr='3em'>You can only mint <b>&nbsp; ONE &nbsp;</b> for they are a 1:1 representation of your wallet</Center>
-            <Center align='center' pl='3em' pr='3em'>They are stored on-chain in the Polygon mainnet network</Center>
-            <Center align='center' pl='3em' pr='3em'>You can think of them as Blockies from the future</Center>
-            <Center align='center' pl='3em' pr='3em'>Drawn as generative art in a beautifully detailed and colorful SVG</Center>
-            <Center align='center' pl='3em' pr='3em'>Using Quadratic and Cubic Bézier curves and leveraging our brain&apos;s ability to remember vertical simmetry</Center>
+            <Center pl='3em' pr='3em'>SVGies are a unique visual representation of your wallet address</Center>
+            <Center pl='3em' pr='3em'>You can only mint ONE because they are a 1:1 representation of your wallet</Center>
+            <Center pl='3em' pr='3em'>They are stored on-chain in the Polygon mainnet network</Center>
+            <Center pl='3em' pr='3em'>You can think of them as Blockies from the future</Center>
+            <Center pl='3em' pr='3em'>Drawn as generative art in a beautifully detailed and colorful SVG</Center>
+            <Center pl='3em' pr='3em'>Using Quadratic and Cubic Bézier curves and leveraging our brain&apos;s ability to remember vertical simmetry</Center>
           </VStack>
         </Center>
-        <Center w='100%' h='18em' >
+        <Center w='100%' h='24em' >
           {(isLoading && <Skeleton><Box w='200' h='200'></Box></Skeleton>)}
           {(isError && <>Error Loading Account</>)}
           {(!isLoading && !isError && !data && <>Please Connect Wallet to Mint or View your SVGie</>)}
           {(!isLoading && !isError && data &&
-            <Center boxSize={240} bg='#ffffffcc' borderRadius={'.75em'}
+            <Center 
+              boxSize={340}
+              bg='#ffffffcc' borderRadius={'.75em'}
               _hover={{ boxShadow: '0 0 8px #ff0080', bg: '#ffffffee' }}>
-              <SVGies address={data?.address} width={200} height={200} fill={`#000`} />
+              <SVGies address={data?.address} width={300} height={300} fill={`#000`} />
             </Center>)}
         </Center>
         <Center w='100%' h='6em' pb='2em'>
@@ -75,9 +77,9 @@ const Home = () => {
             {wallets.map((w, i) => {
               return (
                 <WrapItem key={i}>
-                  <Center boxSize={240} bg='#ffffffcc' borderRadius={'.75em'}
+                  <Center boxSize={190} bg='#ffffffcc' borderRadius={'.75em'}
                     _hover={{ boxShadow: '0 0 8px #ff0080', bg: '#ffffffee' }}>
-                    <SVGies address={w} width={200} height={200} fill={`#000`} />
+                    <SVGies address={w} width={150} height={150} fill={`#000`} />
                   </Center>
                 </WrapItem>
               )
