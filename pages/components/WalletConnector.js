5d3c9e64c43f9221ea448cf0239f004d279d7882
7i7o <mparij@gmail.com>
2022-05-25 21:10:19 -0300

    Fixing Deployment Error 2

--- pages/components/WalletConnector.js
+++ pages/components/WalletConnector.js
@@ -6,16 +6,16 @@ import { chain, createClient, WagmiProvider } from 'wagmi';
 import { apiProvider, configureChains, getDefaultWallets, RainbowKitProvider, lightTheme, darkTheme } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 
-const { chains, provider } = configureChains(
-    // [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-    // [chain.polygon],
-    [chain.polygonMumbai],
-    [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
-);
-const { connectors } = getDefaultWallets({ appName: 'SVGies', chains });
-const wagmiClient = createClient({ autoConnect: true, connectors, provider })
 
 const WalletConnector = ({ Component, pageProps }) => {
+    const { chains, provider } = configureChains(
+        // [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+        // [chain.polygon],
+        [chain.polygonMumbai],
+        [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
+    );
+    const { connectors } = getDefaultWallets({ appName: 'SVGies', chains });
+    const wagmiClient = createClient({ autoConnect: true, connectors, provider })
     const walletTheme = useColorModeValue(lightTheme(), darkTheme())
     return (
         <WagmiProvider client={wagmiClient}>
